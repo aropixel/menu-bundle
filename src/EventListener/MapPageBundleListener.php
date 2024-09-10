b4013d69c20ac1f2dd7965db0c5ed31fde8b6152
Cecile Salon <cecile.salon@aropixel.com>
2024-09-10 18:07:09 +0200

    Bugfix page not translatable

--- src/EventListener/MapPageBundleListener.php
+++ src/EventListener/MapPageBundleListener.php
@@ -1,20 +1,11 @@
 <?php
-/**
- * Créé par Aropixel @2019.
- * Par: Joël Gomez Caballe
- * Date: 16/04/2019 à 15:56
- */
 
 namespace Aropixel\MenuBundle\EventListener;
 
-use Aropixel\MenuBundle\Entity\Menu;
-use Aropixel\PageBundle\Entity\Page;
-use Aropixel\PageBundle\Entity\PageInterface;
-use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
-use Doctrine\ORM\Events;
-use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
+use Aropixel\PageBundle\Entity\PageInterface;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
 
 class MapPageBundleListener
@@ -24,7 +15,10 @@ class MapPageBundleListener
      * @param bool $isPageEnabled
      * @param string $entityName
      */
-    public function __construct(private $isPageEnabled, private $entityName)
+    public function __construct(
+        private bool $isPageEnabled,
+        private string $entityName,
+    )
     {
     }
 
@@ -35,7 +29,7 @@ class MapPageBundleListener
         /** @var ClassMetadataInfo $metadata */
         $metadata = $eventArgs->getClassMetadata();
 
-        if ($this->isPageEnabled && $metadata->getName()==$this->entityName) {
+        if ($this->isPageEnabled && $metadata->getName() == $this->entityName) {
 
             $metadata->mapManyToOne(['fieldName' => 'page', 'targetEntity' => PageInterface::class]);
 
